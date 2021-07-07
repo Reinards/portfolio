2f2128f81a79ddf072ae998d792d4bdda5d1771d
Reinards Saulitis <reinardssaulitis@gmail.com>
2021-07-07 14:31:29 +0300

    Almsot done

--- components/projects/project-list-item.js
+++ components/projects/project-list-item.js
@@ -13,16 +13,14 @@ class ProjectListItem extends Component {
     render() {
         return (
             <Link href={this.props.data.projectHref}>
-                <a>
-                    <div className={css.wrapper} style={{
-                        backgroundImage: "url('/projects/"+ this.props.data.url +"')"
-                    }}>
-                        <div className={css.tint}></div>
-                        <h2 className={css.title}>{this.props.data.title}</h2>
-                        <span className={css.link}>{this.props.data.href}</span>
-                        <div className={css.hover}>
-                            <Image src={Book}/>
-                        </div>
+                <a className={css.wrapper} style={{
+                    backgroundImage: "url('/projects/"+ this.props.data.url +"')"
+                }}>
+                    <div className={css.tint}></div>
+                    <h2 className={css.title}>{this.props.data.title}</h2>
+                    <span className={css.link}>{this.props.data.href}</span>
+                    <div className={css.hover}>
+                        <Image src={Book}/>
                     </div>
                 </a>
             </Link>
